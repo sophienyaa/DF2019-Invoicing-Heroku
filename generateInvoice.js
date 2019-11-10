1249262341ce9822243d230e636a0991b2284e8b
Sophie Wheeler <me@sophi.ee>
2019-11-10 21:40:41 +0000

    major refactor, split to multiple files

---
+++ generateInvoice.js
@@ -0,0 +1,188 @@
+function generateHeader(doc) {
+  doc
+    .image("logo.png", 50, 45, { width: 50 })
+    .fillColor("#444444")
+    .fontSize(20)
+    .text("OVO Energy", 110, 57)
+    .fontSize(10)
+    .text("OVO Energy", 200, 50, { align: "right" })
+    .text("1 Rivergate", 200, 65, { align: "right" })
+    .text("Bristol  BS16ED", 200, 80, { align: "right" })
+    .text("United Kingdom", 200, 95, { align: "right" })
+    .moveDown();
+}
+
+function generateCustomerInformation(doc, invoice) {
+  doc
+    .fillColor("#444444")
+    .fontSize(20)
+    .text("Invoice", 50, 160);
+
+  generateHr(doc, 185);
+
+  const customerInformationTop = 200;
+
+  doc
+    .fontSize(10)
+    .text("Invoice Number:", 50, customerInformationTop)
+    .font("Helvetica-Bold")
+    .text(invoice.Invoice_Number__c, 150, customerInformationTop)
+    .font("Helvetica")
+    .text("Invoice Date:", 50, customerInformationTop + 15)
+    .text(formatDate(new Date()), 150, customerInformationTop + 15)
+    .text("Balance Due:", 50, customerInformationTop + 30)
+    .text(
+      formatCurrency(invoice.subtotal - invoice.paid),
+      150,
+      customerInformationTop + 30
+    )
+
+    .font("Helvetica-Bold")
+    .text(invoice.Company_Name__c, 300, customerInformationTop)
+    .font("Helvetica-Bold")
+    .text(invoice.Customer_Name__c, 300, customerInformationTop + 15)
+    .font("Helvetica")
+    .text(invoice.Address_Street__c, 300, customerInformationTop + 30)
+    .text(
+      invoice.City__c +
+        ", " +
+        invoice.Postcode__c +
+        ", " +
+        invoice.Country__c,
+      300,
+      customerInformationTop + 45
+    )
+    .moveDown();
+
+  generateHr(doc, 267);
+}
+
+function generateInvoiceTable(doc, invoice) {
+  let i;
+  const invoiceTableTop = 330;
+
+  doc.font("Helvetica-Bold");
+  generateTableRow(
+    doc,
+    invoiceTableTop,
+    "Item",
+    "Description",
+    "Unit Cost",
+    "Quantity",
+    "Line Total"
+  );
+  generateHr(doc, invoiceTableTop + 20);
+  doc.font("Helvetica");
+
+  for (i = 0; i < invoice.items.length; i++) {
+    const item = invoice.items[i];
+    const position = invoiceTableTop + (i + 1) * 30;
+    generateTableRow(
+      doc,
+      position,
+      item.productCode,
+      item.lineDescription,
+      formatCurrency(item.itemCost / item.quantity),
+      item.quantity,
+      formatCurrency(item.itemCost)
+    );
+
+    generateHr(doc, position + 20);
+  }
+
+  const subtotalPosition = invoiceTableTop + (i + 1) * 30;
+  generateTableRow(
+    doc,
+    subtotalPosition,
+    "",
+    "",
+    "Subtotal",
+    "",
+    formatCurrency(invoice.subtotal)
+  );
+
+  const paidToDatePosition = subtotalPosition + 20;
+  generateTableRow(
+    doc,
+    paidToDatePosition,
+    "",
+    "",
+    "Paid To Date",
+    "",
+    formatCurrency(invoice.paid)
+  );
+
+  const duePosition = paidToDatePosition + 25;
+  doc.font("Helvetica-Bold");
+  generateTableRow(
+    doc,
+    duePosition,
+    "",
+    "",
+    "Balance Due",
+    "",
+    formatCurrency(invoice.subtotal - invoice.paid)
+  );
+  doc.font("Helvetica");
+}
+
+function generateFooter(doc) {
+  doc
+    .fontSize(10)
+    .text(
+      "Payment is due within 30 days. Thank you for your business.",
+      50,
+      780,
+      { align: "center", width: 500 }
+    );
+}
+
+function generateTableRow(
+  doc,
+  y,
+  item,
+  description,
+  unitCost,
+  quantity,
+  lineTotal
+) {
+  doc
+    .fontSize(10)
+    .text(item, 50, y)
+    .text(description, 150, y)
+    .text(unitCost, 280, y, { width: 90, align: "right" })
+    .text(quantity, 370, y, { width: 90, align: "right" })
+    .text(lineTotal, 0, y, { align: "right" });
+}
+
+function generateHr(doc, y) {
+  doc
+    .strokeColor("#aaaaaa")
+    .lineWidth(1)
+    .moveTo(50, y)
+    .lineTo(550, y)
+    .stroke();
+}
+
+function formatCurrency(amount) {
+  return "£" + amount;
+}
+
+function formatDate(date) {
+  const day = date.getDate();
+  const month = date.getMonth() + 1;
+  const year = date.getFullYear();
+
+  return year + "/" + month + "/" + day;
+}
+
+function generateInvoice(doc, invoice) {
+    generateHeader(doc);
+    generateCustomerInformation(doc, invoice);
+    generateInvoiceTable(doc, invoice);
+    generateFooter(doc);
+}
+
+module.exports = {
+    generateInvoice
+};
--- index.js
+++ index.js
@@ -1,56 +1,66 @@
+//Libraries
 const jsforce = require('jsforce');
 const PDFDocument = require("pdfkit");
-const { Base64Encode } = require('base64-stream');
 const express = require('express');
+const { Base64Encode } = require('base64-stream');
+const { generateInvoice } = require("./generateInvoice.js");
 
+//Env Vars
 const username = process.env.SF_USERNAME;
 const password = process.env.SF_PASSTOKEN;
 const invoiceReqTopic = process.env.SF_INV_REQ;
 const invoiceResTopic = process.env.SF_INV_RES;
 const port = process.env.PORT || 3000;
 
+//Connection & Express app
 var conn = new jsforce.Connection({});
 const app = express();
 
-app.get('/', (req, res) => res.send('Invoice Service Running!'))
-app.listen(port, () => console.log(`Invoice Service running on port ${port}!`))
+//Setup app, login to salesforce and begin listening for events
+function begin() {
+  app.get('/', (req, res) => res.send('Invoice Service Running!'))
+  app.listen(port, () => console.log(`Invoice Service running on port ${port}!`))
 
-conn.login(username, password, function(err, userInfo) {
-  if (err) { return console.error(err); }
-  console.log(userInfo);
-  conn.streaming.topic("/event/" + invoiceReqTopic).subscribe(function(message) {
-      processMessage(message);
+  conn.login(username, password, function(err, userInfo) {
+    if (err) { return console.error(err); }
+    console.log(userInfo);
+    conn.streaming.topic("/event/" + invoiceReqTopic).subscribe(function(message) {
+      processGenerateInvoicePDFEvent(message.payload);
+    });
   });
-});
-
-function processMessage(message) {
-    console.dir(message.payload);
-
-    const customerInfo = JSON.parse(message.payload.Customer_Information__c);
-    const invoiceItems = JSON.parse(message.payload.Line_Item_Information__c);
-    const invoice = {
-        customerInfo: customerInfo,
-        items: invoiceItems
-    }
-    
-    createInvoice(invoice);
 }
 
-function createInvoice(invoice) {
+//Fire the invoice return event defined in SF_INV_RES env var
+function fireInvoicePDFEvent(invoiceBase64, recordId, invoiceNumber) {
+
+  var invoicePDFEvent = {
+      Invoice_PDF_Base64__c: invoiceBase64, 
+      Related_Object_Id__c : recordId, 
+      Invoice_Number__c: invoiceNumber
+  };
+
+  conn.sobject(invoiceResTopic).create(invoicePDFEvent, function(err, ret) {
+      if (err || !ret.success) { return console.error(err, ret); }
+      console.log("Created record id : " + ret.id);
+  });
+
+}
+
+//Process the incoming event defined in SF_INV_REQ env var to generate an invoice PDF
+function processGenerateInvoicePDFEvent(invoice) {
+
   let doc = new PDFDocument({ size: "A4", margin: 50 });
   var finalString = '';
   invoice.subtotal = 0;
   invoice.paid = 0;
 
+  invoice.items = JSON.parse(invoice.Invoice_Line_Item_JSON__c);
 
   invoice.items.forEach(i => {
     invoice.subtotal += i.itemCost * i.quantity;
   })
 
-  generateHeader(doc);
-  generateCustomerInformation(doc, invoice);
-  generateInvoiceTable(doc, invoice);
-  generateFooter(doc);
+  generateInvoice(doc, invoice);
 
   var stream = doc.pipe(new Base64Encode());  
   doc.end();
@@ -60,200 +70,9 @@ function createInvoice(invoice) {
   });
   
   stream.on('end', function() {
-    sendResponse(finalString, invoice.customerInfo.oppId, invoice.customerInfo.invoiceNumber); 
+    fireInvoicePDFEvent(finalString, invoice.Source_Record_Id__c, invoice.Invoice_Number__c); 
   });
 
 }
 
-function sendResponse(finalString, oppId, invoiceNumber) {
-
-    var response = {
-        Invoice_PDF_Base64__c: finalString, 
-        Related_OppId__c : oppId, 
-        Invoice_Number__c: invoiceNumber
-    };
-
-    conn.sobject(invoiceResTopic).create(response, function(err, ret) {
-        if (err || !ret.success) { return console.error(err, ret); }
-        console.log("Created record id : " + ret.id);
-    });
-
-}
-
-function generateHeader(doc) {
-  doc
-    .image("logo.png", 50, 45, { width: 50 })
-    .fillColor("#444444")
-    .fontSize(20)
-    .text("OVO Energy", 110, 57)
-    .fontSize(10)
-    .text("OVO Energy", 200, 50, { align: "right" })
-    .text("1 Rivergate", 200, 65, { align: "right" })
-    .text("Bristol  BS16ED", 200, 80, { align: "right" })
-    .text("United Kingdom", 200, 95, { align: "right" })
-    .moveDown();
-}
-
-function generateCustomerInformation(doc, invoice) {
-  doc
-    .fillColor("#444444")
-    .fontSize(20)
-    .text("Invoice", 50, 160);
-
-  generateHr(doc, 185);
-
-  const customerInformationTop = 200;
-
-  doc
-    .fontSize(10)
-    .text("Invoice Number:", 50, customerInformationTop)
-    .font("Helvetica-Bold")
-    .text(invoice.customerInfo.invoiceNumber, 150, customerInformationTop)
-    .font("Helvetica")
-    .text("Invoice Date:", 50, customerInformationTop + 15)
-    .text(formatDate(new Date()), 150, customerInformationTop + 15)
-    .text("Balance Due:", 50, customerInformationTop + 30)
-    .text(
-      formatCurrency(invoice.subtotal - invoice.paid),
-      150,
-      customerInformationTop + 30
-    )
-
-    .font("Helvetica-Bold")
-    .text(invoice.customerInfo.companyName, 300, customerInformationTop)
-    .font("Helvetica-Bold")
-    .text(invoice.customerInfo.customerName, 300, customerInformationTop + 15)
-    .font("Helvetica")
-    .text(invoice.customerInfo.addressStreet, 300, customerInformationTop + 30)
-    .text(
-      invoice.customerInfo.city +
-        ", " +
-        invoice.customerInfo.postcode +
-        ", " +
-        invoice.customerInfo.country,
-      300,
-      customerInformationTop + 45
-    )
-    .moveDown();
-
-  generateHr(doc, 267);
-}
-
-function generateInvoiceTable(doc, invoice) {
-  let i;
-  const invoiceTableTop = 330;
-
-  doc.font("Helvetica-Bold");
-  generateTableRow(
-    doc,
-    invoiceTableTop,
-    "Item",
-    "Description",
-    "Unit Cost",
-    "Quantity",
-    "Line Total"
-  );
-  generateHr(doc, invoiceTableTop + 20);
-  doc.font("Helvetica");
-
-  for (i = 0; i < invoice.items.length; i++) {
-    const item = invoice.items[i];
-    const position = invoiceTableTop + (i + 1) * 30;
-    generateTableRow(
-      doc,
-      position,
-      item.productCode,
-      item.lineDescription,
-      formatCurrency(item.itemCost / item.quantity),
-      item.quantity,
-      formatCurrency(item.itemCost)
-    );
-
-    generateHr(doc, position + 20);
-  }
-
-  const subtotalPosition = invoiceTableTop + (i + 1) * 30;
-  generateTableRow(
-    doc,
-    subtotalPosition,
-    "",
-    "",
-    "Subtotal",
-    "",
-    formatCurrency(invoice.subtotal)
-  );
-
-  const paidToDatePosition = subtotalPosition + 20;
-  generateTableRow(
-    doc,
-    paidToDatePosition,
-    "",
-    "",
-    "Paid To Date",
-    "",
-    formatCurrency(invoice.paid)
-  );
-
-  const duePosition = paidToDatePosition + 25;
-  doc.font("Helvetica-Bold");
-  generateTableRow(
-    doc,
-    duePosition,
-    "",
-    "",
-    "Balance Due",
-    "",
-    formatCurrency(invoice.subtotal - invoice.paid)
-  );
-  doc.font("Helvetica");
-}
-
-function generateFooter(doc) {
-  doc
-    .fontSize(10)
-    .text(
-      "Payment is due within 30 days. Thank you for your business.",
-      50,
-      780,
-      { align: "center", width: 500 }
-    );
-}
-
-function generateTableRow(
-  doc,
-  y,
-  item,
-  description,
-  unitCost,
-  quantity,
-  lineTotal
-) {
-  doc
-    .fontSize(10)
-    .text(item, 50, y)
-    .text(description, 150, y)
-    .text(unitCost, 280, y, { width: 90, align: "right" })
-    .text(quantity, 370, y, { width: 90, align: "right" })
-    .text(lineTotal, 0, y, { align: "right" });
-}
-
-function generateHr(doc, y) {
-  doc
-    .strokeColor("#aaaaaa")
-    .lineWidth(1)
-    .moveTo(50, y)
-    .lineTo(550, y)
-    .stroke();
-}
-
-function formatCurrency(amount) {
-  return "£" + amount;
-}
-
-function formatDate(date) {
-  const day = date.getDate();
-  const month = date.getMonth() + 1;
-  const year = date.getFullYear();
-
-  return year + "/" + month + "/" + day;
-}
+begin();
